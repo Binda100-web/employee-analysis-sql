6dada35829d5664ee0e7a2c169e8064c301a58d8
Phumlani Binda <phumlanibinda@hotmail.com>
2025-08-14 13:39:41 +0200

    new tak

---
+++ New_deploy.sh
@@ -0,0 +1,21 @@
+git clone https://github.com/TechPerformanceCenterStudents/peermissions-task-3-Binda100-web
+cd peermissions-task-3-Binda100-web
+git init
+git add .
+git commit -m "New_deploy.sh"
+git remote add origin https://github.com/TechPerformanceCenterStudents/peermissions-task-3-Binda100-web
+git push origin main
+
+
+
+
+
+
+
+
+
+
+
+^G Help      ^O Write Out ^F Where Is  ^K Cut       ^T Execute   ^C Location
+^X Exit      ^R Read File ^\ Replace   ^U Paste     ^J Justify   ^/ Go To Line
+
